a489f476b82c7ea66efd85132898fe7cd8f2ff52
Faisal <faisalqodri@idn.ac.id>
2026-01-16 09:51:45 +0700

    Commit for Fav Icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "MyCalla Studio | Creative Agency",
   description: "Shaping digital identities through creative excellence",
+  icons: {
+    icon: "/logo.png",
+  },
 };
 
 export default function RootLayout({
